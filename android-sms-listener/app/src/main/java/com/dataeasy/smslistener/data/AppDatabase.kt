8acd40fc88d6a85c196d1d6e168f0d661c5f5c84
Oteng Kwame Bismark <otengbismark7@gmail.com>
2026-02-22 14:23:14 +0000

    Fix: Add TypeConverters for Status enum (fixes ClassCastException)

--- android-sms-listener/app/src/main/java/com/dataeasy/smslistener/data/AppDatabase.kt
+++ android-sms-listener/app/src/main/java/com/dataeasy/smslistener/data/AppDatabase.kt
@@ -2,12 +2,30 @@ package com.dataeasy.smslistener.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+/**
+ * Type converters for Room database
+ */
+class Converters {
+    @TypeConverter
+    fun fromStatus(status: MoMoTransaction.Status): String {
+        return status.name
+    }
+    
+    @TypeConverter
+    fun toStatus(value: String): MoMoTransaction.Status {
+        return MoMoTransaction.Status.valueOf(value)
+    }
+}
 
 @Database(
     entities = [MoMoTransaction::class, UnparsedSms::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun momoTransactionDao(): MoMoTransactionDao
     abstract fun unparsedSmsDao(): UnparsedSmsDao
